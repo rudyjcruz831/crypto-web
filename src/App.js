d2138f94765617784e704c8445c3efb2f4471442
rudcruz_csumb.edu <rudcruz@csumb.edu>
2021-11-08 12:52:44 -0800

    added routes to be able to have multiple pages :

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
-import Navigation from './components/Navigation'
-import Info  from './components/Info'
-import React, { useState, useEffect } from 'react'
+import Home from './Home';
+import About from './About';
+import {Route, Routes} from 'react-router-dom'
+import React from 'react';
 
 function App() {
-  const [crypto, setCrypto] = useState([])
-
-  useEffect(() => {
-      const getCrypto = async () => {
-          try {
-            const cryptoFromServer = await fetchCrypto()
-            setCrypto(cryptoFromServer)
-          } catch (e) {
-            console.log(e)
-          }
-          
-          // console.log("inside useEffect")
-      }
-      getCrypto()
-  },[])
-
-  // fetch crypto data
-  const fetchCrypto = async () =>{
-      const res = await fetch("https://cryptic-thicket-61793.herokuapp.com/info")
-      const data = await res.json()
-      console.log(data.Exchanges)
-      return data
-  }
-  
   return (
     <div className="App">
-      <Navigation/>
-      <Info crypto={crypto}/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/about" element={<About/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import Navigation from './components/Navigation'
+import Info  from './components/Info'
+import React, { useState, useEffect } from 'react'
+
+const Home = () => {
+    const [crypto, setCrypto] = useState([])
+
+    useEffect(() => {
+        const getCrypto = async () => {
+            try {
+                const cryptoFromServer = await fetchCrypto()
+                setCrypto(cryptoFromServer)
+            } catch (e) {
+                console.log(e)
+            }
+            // console.log("inside useEffect")
+        }
+        getCrypto()
+    },[])
+
+    // fetch crypto data
+    const fetchCrypto = async () =>{
+        const res = await fetch("https://cryptic-thicket-61793.herokuapp.com/info")
+        const data = await res.json()
+        console.log(data.Exchanges)
+        return data
+    }
+
+    return (
+        <div>
+            <Navigation/>
+            <Info crypto={crypto}/>
+        </div>
+    )
+}
+
+export default Home
